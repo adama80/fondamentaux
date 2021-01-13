0267afad922fe0eead78cae47478e6ad1d6f283e
adama80 <77321786+adama80@users.noreply.github.com>
2021-01-13 14:13:12 +0000

    Add files via upload

---
+++ exercice3.php
@@ -0,0 +1,21 @@
+<?php
+$max=null;
+$moy=null
+$min=null;
+if($chiffre1>$chiffre2){
+    $min=$chiffre1;
+    $max=$chiffre2;
+}
+if ($chiffre2<$chiffre1)
+    $min=$chiffre2
+    $max=$chiffre1
+
+if($chiffre2>=$chiffre3){
+    $min=$chiffre1;
+    $max=$chiffre2;
+}
+ echo "l'ordre decroissant donne $max $moy $min"
+
+
+
+?>
